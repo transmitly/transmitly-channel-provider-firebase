f4b6b9eeca0273d0357c30a8fccf3860dc5976bd
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-02-13 19:19:30 -0700

    Initial Commit

---
+++ FirebaseChannelProviderClient.cs
@@ -0,0 +1,73 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Transmitly.Channel.Push;
+
+namespace Transmitly.ChannelProvider.Firebase
+{
+	internal sealed class FirebaseChannelProviderClient : ChannelProviderClient<IPushNotification>
+	{
+		public FirebaseChannelProviderClient(AppOptions options)
+		{
+			Guard.AgainstNull(options);
+			FirebaseApp.Create(options);
+		}
+
+		public override async Task<IReadOnlyCollection<IDispatchResult?>> DispatchAsync(IPushNotification communication, IDispatchCommunicationContext communicationContext, CancellationToken cancellationToken)
+		{
+			List<Message> messages = new(communication.To.Count);
+			foreach (var recipient in communication.To)
+			{
+				messages.Add(new Message
+				{
+					Data = TryConvertToDictionary(communicationContext.ContentModel?.Model),
+					Notification = new Notification
+					{
+						Title = communication.Title,
+						Body = communication.Body,
+						ImageUrl = communication.ImageUrl
+					},
+					Token = recipient.IfType(AudienceAddress.Types.DeviceToken(), recipient.Value),
+					Topic = recipient.IfType(AudienceAddress.Types.Topic(), recipient.Value)
+				});
+			}
+
+			var response = await FirebaseMessaging.DefaultInstance.SendAllAsync(messages, cancellationToken);
+
+			return response.Responses.Select(m => new FirebaseDispatchResult(m)).ToList();
+		}
+
+		private static Dictionary<string, string?>? TryConvertToDictionary(object? content)
+		{
+			try
+			{
+				if (content == null)
+					return null;
+				var props = content.GetType().GetProperties();
+				var pairDictionary = props.ToDictionary(x => x.Name, x => x.GetValue(content, null)?.ToString());
+				return pairDictionary;
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ FirebaseChannelProviderExtensions.cs
@@ -0,0 +1,47 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using FirebaseAdmin;
+using System;
+using Transmitly.Channel.Configuration;
+using Transmitly.ChannelProvider.Firebase;
+
+namespace Transmitly
+{
+	public static class FirebaseChannelProviderExtensions
+	{
+		private const string FirebaseId = "Google.Firebase";
+		private const string DefaultProviderId = "Default";
+
+		public static string Firebase(this ChannelProviders channelProviders, string? providerId = DefaultProviderId)
+		{
+			Guard.AgainstNull(channelProviders);
+
+			return $"{FirebaseId}.{(!string.IsNullOrWhiteSpace(providerId) ? providerId : DefaultProviderId)}";
+		}
+
+		public static CommunicationsClientBuilder AddFirebaseSupport(this ChannelProviderConfigurationBuilder channelProviderConfiguration, Action<AppOptions> options, string? providerId = null)
+		{
+			var optionObj = new AppOptions();
+			options(optionObj);
+
+			return channelProviderConfiguration.Add(Id.ChannelProvider.Firebase(providerId), new FirebaseChannelProviderClient(optionObj), Id.Channel.PushNotification());
+		}
+
+		public static CommunicationsClientBuilder AddFirebaseSupport(this CommunicationsClientBuilder communicationsClientBuilder, Action<AppOptions> options, string? providerId = null)
+		{
+			return communicationsClientBuilder.ChannelProvider.AddFirebaseSupport(options, providerId);
+		}
+	}
+}
